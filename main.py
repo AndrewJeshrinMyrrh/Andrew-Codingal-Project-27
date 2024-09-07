ce37948b386d1b8b03dda1c6092283399d559eef
AndrewJeshrinMyrrh <nickolas.rajesh@gmail.com>
2024-09-07 07:46:42 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+length = int(input("Enter the length of your password: "))
+
+chars = string.ascii_letters
+chars += string.digits
+chars += string.punctuation
+
+password = ""
+
+for i in range(length):
+    next_character = random.choice(chars)
+    password += next_character
+
+print("Your random password is:", password)
+    
